0b1993a2ef4320ec28aa9c0dfe476da2db046114
jhue8307 <jhue8307@gmail.com>
2026-08-19 00:03:06 +0900

    Add files via upload

---
+++ assets/img/gallery/gallery-list.js
@@ -0,0 +1,33 @@
+// ============================================================
+// ⭐ 갤러리 사진 추가/삭제하는 법 ⭐
+//
+// 1) 이 폴더(assets/img/gallery/photos/)에 사진 파일을 넣는다.
+// 2) 아래 목록에 파일명을 한 줄 추가한다. (따옴표 " ", 줄 끝 쉼표 , 형식만 지켜주면 됨)
+// 3) 끝! index.html이나 다른 파일은 전혀 건드릴 필요 없습니다.
+//    순서를 바꾸고 싶으면 이 목록의 줄 순서만 바꾸면 됩니다.
+//
+// 사진 용량 팁: 원본 그대로(20~30MB) 넣으면 페이지 로딩이 느려집니다.
+// 긴 변 기준 1600px 정도, 파일 용량 200~400KB 정도로 줄여서 넣는 걸 추천해요.
+// (사진 리사이즈가 번거로우면 Claude한테 사진 보내서 부탁하셔도 됩니다)
+// ============================================================
+
+var GALLERY_PHOTOS = [
+  "gallery-01.jpg",
+  "gallery-02.jpg",
+  "gallery-03.jpg",
+  "gallery-04.jpg",
+  "gallery-05.jpg",
+  "gallery-06.jpg",
+  "gallery-07.jpg",
+  "gallery-08.jpg",
+  "gallery-09.jpg",
+  "gallery-10.jpg",
+  "gallery-11.jpg",
+  "gallery-12.jpg",
+  "gallery-13.jpg",
+  "gallery-14.jpg",
+  "gallery-15.jpg",
+  "gallery-16.jpg",
+  "gallery-17.jpg",
+  "gallery-18.jpg"
+];
